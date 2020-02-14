2cbf676720fdb2418295e1d68c1adea202be8b09
Jenny <j.thurnau@hotmail.com>
2020-02-14 13:24:29 -0800

    Added controllers

--- models/boilerPlate.js
+++ models/boilerPlate.js
@@ -6,42 +6,17 @@ const boilerPlateSchema = new Schema(
             type: Date,
             default: () => new Date()
         },
-        // exercises: [
-        //     {
-        //         type: {
-        //             type: String,
-        //             trim: true,
-        //             required: "Enter an exercise type"
-        //         },
-        //         name: {
-        //             type: String,
-        //             trim: true,
-        //             required: "Enter an exercise name"
-        //         },
-        //         duration: {
-        //             type: Number,
-        //             required: "Enter an exercise duration in minutes"
-        //         }
-        //     }
-        // ],
         
         user:{
             name: String,
             password: String,
             location: String,
             email: String,
-
-            completedActivites [
-                {
+            completedActivites: {
                     placeholder: null
                     // insert completed activities via mongo version of join.
                 }
-            ]
-
-
         }
-
-
     },
 );
 const boilerPlate = mongoose.model("lifestyle", boilerPlateSchema);
