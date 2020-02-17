7da813ab64274ebf6972af327c28b94cd0693b26
Gregory Reneris <greg@reneris.com>
2020-02-17 14:45:03 -0800

    comments.

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const collections = ["user"];
 const tmApikey = "dwXD5AKGG1cYnioNAAh1PSKaTZu2TIVN";
 const tmSize = 100; 
 const tmUrl = "https://app.ticketmaster.com/discovery/v2/events.json";
-const tmCity = "seattle";
+const tmCity = "seattle"; // this will be pulled from user location later.
 const hkApikey = "200685387-0d1f511c4df4599326d988945a93ebf8";
-const hkLocation = "lat=47.6045335&lon=-122.3531904"
+const hkLocation = "lat=47.6045335&lon=-122.3531904" // this will be pulled from user location later.
 const hkRadius = 30;
 const hkMaxResults = 5;
 const hkUrl = "https://www.hikingproject.com/data";
@@ -26,7 +26,7 @@ const hkUrl = "https://www.hikingproject.com/data";
 // const db = mongojs(databaseUrl, collections);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lifestyle", { useNewUrlParser: true, useUnifiedTopology: true});
 
-// the below is probably wrong.
+
 const db = mongojs(databaseUrl, collections);
 db.on("error", error => {
   console.log("Database Error:", error);
@@ -57,14 +57,14 @@ app.use(session({ secret: "something secret here", resave: true, saveUninitializ
 
 // BELOW IS AUTHENTICATION **********************
 app.post("/api/auth/signup", (req, res) => {
-  db.User.create(req.body).then(userData => {
+  db.User.create(req.body).then(userData => {         // this needs to switch to Mongo stuff.
     res.json(userData);
   })
 })
 app.post("/api/auth/login", (req, res) => {
-  db.User.findOne({
+  db.User.findOne({                                   // double check this with Mongo commands as well.
     where: {
-      name: req.body.name
+      name: req.body.name                             // for our sign up should this be name or email or username?
     }
   }).then(dbUser=>{
     if(bcrypt.compareSync(req.body.password,dbUser.password)){
@@ -115,7 +115,7 @@ app.get('/api/events' , (req,res)=>{
 
 
 app.get('/api/hikes' , (req,res)=>{
-  console.log("We want to know!");
+  // console.log("We want to know!");
   let maxDuration = 4*60;
 
   let finalurl = `${hkUrl}/get-trails?key=${hkApikey}&maxDistance=${hkRadius}&maxResult=${hkMaxResults}&${hkLocation}`; 
