002955aa85a2a56ce7b358169f9e632589e77dcc
Gregory Reneris <greg@reneris.com>
2020-02-21 18:36:33 -0800

    update.

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,6 +1,7 @@
 // add or remove activities
 const hikeController = require("./hikeController")
 const tm = require("./ticketMasterController")
+const db = require("../models")
 
 let users = ""
 
@@ -8,42 +9,43 @@ function addEvent(req, res) {
     let [type, id] = req.params.id.split(":");
 
     if (type === "hike") {
-        hikeController.getHikeById(id).then(hike => {
-            saveActivity(hike, res)
+        hikeController.getHikeById(id).then(    hike => {
+            saveActivity(hike, res,req) // joe aded (req) to this because we weren't passing the object all the way through and thus weren't getting the user.session either.
         })
     } else if (type === "event") {
         tm.getEventById(id).then(event => {
-            saveActivity(event, res)
+            saveActivity(event, res,req)
         })
     } else {
         res.sendStatus(403);
     }
 }
 
-function saveActivity (activity, res) {
+function saveActivity (activity, res,req) {
     activity.save(err => {
         if (err) {
             console.log("Save failed " + err);
         }
+        console.log(activity)
     // users.completedActivites.save
-    //         // we need to get users defined by the get call so the server knows which user to attach the activity to. It is crashing because of this.
-
+            // we need to get users defined by the get call so the server knows which user to attach the activity to. It is crashing because of this.
+    db.User.findOneAndUpdate( // needed to call db.User instead of this.findOneAndUpdate. Joe also was like this design is crazy!
     // this.findOneAndUpdate(
-    //     console.log("Got here to findoneandupdate ******************************************************"),
-    //     console.log(req.session.user.id),
-    //     {"_id": this.req.session.user.id }, 
+        // console.log("Got here to findoneandupdate ******************************************************"),
+        // console.log(req.session.user.id),
+        {"_id": req.session.user.id }, 
     
-    //     {$push: {"completedActivites": {_id: activity._id, id: activity.id, activityName: activity.activityName, description : activity.description, url : activity.url, imgurl :activity.imgurl, type : activity.type, latitude : activity.latitude, longitude : activity.longitude, travelTime : activity.travelTime, date : activity.date, _v: activity._v}}},
-    //     {new: true},
-    //     function(err, model){
-    //        if (err){
-    //          console.log("ERROR: ", err);
-    //          res.send(500, err);
-    //        }else{
-    //          res.status(200).send(model);
-    //        }
-    //       }
-    //     );
+        {$push: {"completedActivites": {_id: activity._id, id: activity.id, activityName: activity.activityName, description : activity.description, url : activity.url, imgurl :activity.imgurl, type : activity.type, latitude : activity.latitude, longitude : activity.longitude, travelTime : activity.travelTime, date : activity.date, _v: activity._v}}},
+        {new: true},
+        function(err, model){
+           if (err){
+             console.log("ERROR: ", err);
+             res.send(500, err);
+           }else{
+             res.status(200).send(model);
+           }
+          }
+        );
 
     // (err => {7
     //     if (err) {
@@ -51,7 +53,7 @@ function saveActivity (activity, res) {
     //     }
     // }) 
     })
-    res.sendStatus(200); // in future change to redirect.
+    // res.sendStatus(200); // in future change to redirect.
 
 }
 // get current session user
