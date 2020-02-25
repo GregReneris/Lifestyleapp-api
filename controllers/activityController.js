cd1e9fd62255af46024043207f431cd4d7e4707c
Gregory Reneris <greg@reneris.com>
2020-02-24 18:12:11 -0800

    closer to getting the rating number to change.

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -29,10 +29,10 @@ function saveActivity(activity, res, req) {
         // we need to get users defined by the get call so the server knows which user to attach the activity to. It is crashing because of this.
         db.User.findOneAndUpdate( // needed to call db.User instead of this.findOneAndUpdate. Joe also was like this design is crazy!
             // this.findOneAndUpdate(
-                // console.log(req.session.user.id),
+            // console.log(req.session.user.id),
             { "_id": req.session.user.id },
 
-            { $push: { "completedActivites": { _id: activity._id, id: activity.id, activityName: activity.activityName, description: activity.description, url: activity.url, imgurl: activity.imgurl, type: activity.type, latitude: activity.latitude, longitude: activity.longitude, travelTime: activity.travelTime, date: activity.date, _v: activity._v } } },
+            { $push: { "completedActivites": { _id: activity._id, id: activity.id, activityName: activity.activityName, description: activity.description, url: activity.url, imgurl: activity.imgurl, type: activity.type, latitude: activity.latitude, longitude: activity.longitude, travelTime: activity.travelTime, date: activity.date, _v: activity._v, rating: activity.rating } } },
             { new: true },
             function (err, model) {
                 console.log("Got here to findoneandupdate ******************************************************")
@@ -53,30 +53,54 @@ function saveActivity(activity, res, req) {
 function deleteActivity(req, res, activity) {
     console.log("IN DELETE ACTIVITY")
     console.log(activity)
-    console.log ("BELOW IS REQ")
+    console.log("BELOW IS REQ")
     // console.log (req)
-        db.User.findOneAndUpdate( 
-            { "_id": req.session.user.id },
+    db.User.findOneAndUpdate(
+        { "_id": req.session.user.id },
 
-            { $pull: { "completedActivites": {id: activity} } },
-            function (err, model) {
-                console.log("Got here to findoneandpull ******************************************************")
-                console.log (model);
+        { $pull: { "completedActivites": { id: activity } } },
+        function (err, model) {
+            console.log("Got here to findoneandpull ******************************************************")
+            console.log(model);
 
-                if (err) {
-                    console.log("ERROR: ", err);
-                    res.send(500, err);
-                } else {
-                    res.status(200).send(model);
-                }
+            if (err) {
+                console.log("ERROR: ", err);
+                res.send(500, err);
+            } else {
+                res.status(200).send(model);
             }
+        }
     );
 }
-    // res.sendStatus(200); // in future change to redirect.
+// res.sendStatus(200); // in future change to redirect.
 
+
+function setStars(req, res) {
+    // console.log(id, value);
+    console.log (req.session.user)
+    console.log(req.body)
+
+    db.User.findOne(
+        { _id: req.session.user.id } )
+        .then(user => {
+            console.log("GOT HERE tO USER")
+            console.log(user)
+            user.update({"completedActivites._id" : Number(req.body.id) }, {"$set": {"completedActivites.$.rating": Number(req.body.value)}})
+        } )
+
+            // if (err) {
+            //     console.log("ERROR: ", err);
+            //     res.send(500, err);
+            // } else {
+            //     res.status(200).send(model);
+            // }
+};
+    
 
 
 module.exports = {
     addEvent,
-    deleteActivity
+    deleteActivity,
+    setStars
+
 }
